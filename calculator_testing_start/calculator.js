d5cbd967be01d9e4d5d7be964dc6cf7a302a75d2
Michelle Tambi <michelletambi22@gmail.com>
2022-05-31 14:55:07 +0100

    completed functions in the calculator file

--- calculator_testing_start/calculator.js
+++ calculator_testing_start/calculator.js
@@ -13,11 +13,18 @@ console.log(multiply(2,3))
 const divide = (a,b ) => a /b;
 console.log(divide(3,1))
 
-const modulus = null;
+function modulus(a, b){
+    return a % b;
+};
+console.log(modulus(5,3));
 
-const even = null;
+const even = function(a){
+    return (a % 2) == 0;
+};
+console.log(even(5))
 
-const odd = null;
+const odd = (a) => a % 2 > 0;
+console.log(odd(4))
 
 module.exports = { 
     sum, 
