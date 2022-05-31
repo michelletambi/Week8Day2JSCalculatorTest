f5d290462330f57380ee7bb957b5b6998942c243
Michelle Tambi <michelletambi22@gmail.com>
2022-05-31 14:47:52 +0100

    completed subtract function

---
+++ calculator_testing_start/calculator.js
@@ -0,0 +1,28 @@
+const sum = function(a, b){
+    return a + b;
+};
+
+const subtract = function(a, b){
+    return a - b;
+};
+console.log(subtract(10,5))
+
+const multiply = (a, b) => a * b;
+
+const divide = null;
+
+const modulus = null;
+
+const even = null;
+
+const odd = null;
+
+module.exports = { 
+    sum, 
+    subtract, 
+    multiply, 
+    divide, 
+    modulus, 
+    even, 
+    odd 
+};
