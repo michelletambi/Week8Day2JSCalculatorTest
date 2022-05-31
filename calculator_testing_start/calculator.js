398689b0158101af3e5fc15fd94f0c341aaebb90
Michelle Tambi <michelletambi22@gmail.com>
2022-05-31 16:00:49 +0100

    added test for add, subtract and multiply

--- calculator_testing_start/calculator.js
+++ calculator_testing_start/calculator.js
@@ -5,26 +5,26 @@ const sum = function(a, b){
 const subtract = function(a, b){
     return a - b;
 };
-console.log(subtract(10,5))
+// console.log(subtract(10,5))
 
 const multiply = (a, b) => a * b;
-console.log(multiply(2,3))
+// console.log(multiply(2,3))
 
 const divide = (a,b ) => a /b;
-console.log(divide(3,1))
+// console.log(divide(3,1))
 
 function modulus(a, b){
     return a % b;
 };
-console.log(modulus(5,3));
+// console.log(modulus(5,3));
 
 const even = function(a){
     return (a % 2) == 0;
 };
-console.log(even(5))
+// console.log(even(5))
 
 const odd = (a) => a % 2 > 0;
-console.log(odd(4))
+// console.log(odd(4))
 
 module.exports = { 
     sum, 
--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -16,25 +16,62 @@ describe('sum', () => {
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
+  test('can add two large positive numbers', () => { 
+    // arrange
+    // act
+    const actual = sum(250, 550)
+    const expected = 800;
+    // assert
+    expect(actual).toBe(expected);
+  });
+
+  test('can add two negative numbers', () => {
+    const actual = sum(-2, -2);
+    const expected = -4;
+    expect(actual).toBe(expected);
     
   });
 
-  test.skip('can add two negative numbers', () => {
-    
-  });
-
-  test.skip('can add zero', () => {
+  test('can add zero', () => {
+    const actual = sum(0, 5);
+    const expected = 5;
+    expect(actual).toBe(expected);
     
   });
 
 });
 
 describe('subtract', () => {
+  test('can subtract small numbers', () => {
+    // arrange
+    
+    // act
+    const actual = subtract(10, 2);
+    const expected = 8;
+    // assert
+    expect(actual).toBe(expected);
+  });
+
+  test('can subtract when b > a', () => {
+    const actual = subtract(10, 20);
+    const expected = -10;
+    expect(actual).toBe(expected);
+  })
 
 });
 
 describe('multiply', () => {
+  test('can multiply small numbers', () =>{
+    const actual = multiply(3, 4);
+    const expected = 12;
+    expect(actual).toBe(expected);
+  });
+
+  test('can multiply big numbers', () => {
+    const actual = multiply(212, 134);
+    const expected = 28408;
+    expect(actual).toBe(expected);
+  })
 
 });
 
